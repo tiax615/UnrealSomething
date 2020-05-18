785619144a7b26da7a669575b9a3777cb54965d5
caidu <caidu@minieye.cc>
2020-05-18 16:08:50 +0800

    20200518 start

---
+++ Source/UnrealSomething/UnrealSomethingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealSomethingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSOMETHING_API AUnrealSomethingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
